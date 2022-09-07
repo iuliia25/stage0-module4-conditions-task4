5db7025259740ec36843e58572436752145ef665
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-07 18:17:38 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int maxNumber = first;
+        if (second >= maxNumber) {
+            maxNumber = second;
+        }
+        if (third >= maxNumber) {
+            maxNumber = third;
+        }
+        System.out.println(maxNumber);
     }
 }
